457e54b652e4dd251ce921bbab07200045f4de6d
Alexmitter <compaq@live.de>
2018-10-21 16:52:21 +0200

    New grapical menu and language pack feature

---
+++ language/english.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+#language pack for the dialoge menu
+
+export TITLE="osulazer build and run script"
+export RUN="Run osulazer"
+export COMPILE="Clone and Compile osulazer"
+export PKG="Make a Linux Executable(not needed)"
+export CHOOSE="Choose one of the following options:"
---
+++ start.sh
@@ -0,0 +1,51 @@
+#/bin/bash
+
+. ./english.sh
+
+OPTIONS=(1 "$RUN" 
+         2 "$COMPILE" 
+         3 "$DEPENDS")
+         
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+
+CHOICE=$(dialog --clear \
+                --backtitle "$TITLE" \
+                --title "$TITLE" \
+                --menu "$CHOOSE" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+
+
+clear
+case $CHOICE in
+        1)
+			if [ -d "scripts/osu" ]; then
+				cd scripts/
+				sh run_osulazer.sh
+			else
+				echo "No osulazer found, run build first"
+			fi
+            ;;
+        2)
+			if [ -d "scripts/osu" ]; then
+				cd scripts/
+				dialog --title "old osulazer found" --backtitle "Do you want to remove the old osulazer to build a new one" --yesno "Are you sure you want to permanently delete your old osu build" 7 60
+				response=$?
+				case $response in
+					0) sudo rm -r osu/ && sh build_osulazer.sh;;
+					1) echo "osu not deleted.";;
+					255) echo "[ESC] key pressed.";;
+				esac
+			else
+				cd scripts/
+				sh build_osulazer.sh
+			fi            
+            ;;
+        3)
+            echo "Not implemented yet"
+            ;;
+esac
