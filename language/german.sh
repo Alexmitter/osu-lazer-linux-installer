4673ff271c8d6dfee362799f28fc34488b027cae
Alexander <37543796+Alexander88207@users.noreply.github.com>
2018-12-15 13:01:47 +0100

    Add files via upload

---
+++ language/german.sh
@@ -0,0 +1,36 @@
+#/bin/sh
+
+#language pack for the dialoge menu
+
+#Generic
+NOTIMPLEMENTED="Noch nicht implementiert".
+INSTALLPACKAGES="Installation"
+
+#start.sh
+TITLE="osu!lazer Skript zum erstellen & starten".
+WINDOW="Menü".
+RUN="Starte osu!lazer".
+COMPILE="Klonen und Kompilieren Sie osu!lazer".
+PKG="Linux ausführbar machen (nicht erforderlich)".
+DEPENDS="Abhängigkeiten installieren".
+CHOOSE="Wählen Sie eine der folgenden Optionen:".
+QUICKFIXES="Quickfixes".
+QUICKFIXES2="Quickfixes für häufige Probleme".
+NOOSUFOUND="Kein Osulazer gefunden, zuerst Build ausführen".
+OLDOSUFOUND="alter Osulazer gefunden".
+BUILDNEWASK="Möchtest du den alten Osulazer entfernen, um einen neuen zu bauen?"
+SURETODELETE="Bist du sicher, dass du deinen alten osu-Build dauerhaft löschen willst ("benötigt root") "
+OSUNOTREMOVED="osu nicht gelöscht".
+ESCPRESSED="[ESC] Taste gedrückt".
+
+#dependences.sh
+DOTNETDL="Dotnet 2.1(Platzhalter)".
+DEBDEP="Gemeinsame Debian/Ubuntu-Abhängigkeiten erforderlich".
+INSTALLBASSLIB="Installation der Bass Libs aus dem von Ihnen entwickelten Osulazer(Platzhalter)".
+MULSCREEN="Paket zur Behebung von Multidisplay-Problemen".
+
+#Quickfixes.sh
+NOTHINGHERE="Nichts im Moment hier".
+DELFRAMEWORKINI="framework.ini entfernen, Fixierung nicht gestartet".
+DELFRAMEWORKINI2="framework.ini aus ~/.local/share/osu entfernen".
+
