9d80635972bd942007f1dc689c40d723c991110c
Alexmitter <compaq@live.de>
2018-12-16 16:06:22 +0100

    Add initial scripts for automatic dependency installation

---
+++ depscripts/ubuntu18.04/ubuntu18.04.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Dependency Install Script for ubuntu 18.04
+#
+# It will install the dependencies AND Check if they are correctly installed.
+# 
+# Those scripts should contain functions that deal with the installation process of every dependency part seperatly.
+#
+# This script should be run as root.
+#
+
+function Dotnet
+{
+	https://github.com/Alexmitter/osu-lazer-linux-installer.git
+}
+
+function BassDeps
+{
+
+}
+
