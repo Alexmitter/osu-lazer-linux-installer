64c59d811a13ff432509dda57588bdd74e0df704
Alexmitter <compaq@live.de>
2018-12-16 16:38:15 +0100

    Initial State of Ubuntu 18.04 Script

--- depscripts/ubuntu18.04/ubuntu18.04.sh
+++ depscripts/ubuntu18.04/ubuntu18.04.sh
@@ -11,11 +11,47 @@
 
 function Dotnet
 {
-	https://github.com/Alexmitter/osu-lazer-linux-installer.git
+	read -r -p "This Script is activating the Universe Repo. Are you sure? [y/N] " response
+	response=${response,,}    # tolower
+	if [[ "$response" =~ ^(yes|y)$ ]] ; then		
+		wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+		dpkg -i packages-microsoft-prod.deb
+		echo "Dotnet Repo added"
+	
+		add-apt-repository universe
+		apt install apt-transport-https -y
+		apt update
+		apt install dotnet-sdk-2.2 -y
+	
+		if ! [ -x "$(command -v dotnet)" ]; then
+			echo 'Error: dotnet core sdk could not be installed' >&2
+			echo 'If you are running the correct Distro Version for this script, make a issue on github' >&2
+		exit 1
+	fi
+
+fi
 }
 
 function BassDeps
 {
-
+	echo "Install libbass shared object"
+	if [ ! -f /usr/lib/libbass.so ]; then
+		echo "File is not placed, installing..."
+		cp bin/libbass.so /usr/lib/
+	fi
+	
+	echo "Install libbass_fx shared object"
+	if [ ! -f /usr/lib/libbass_fx.so ]; then
+		echo "File is not placed, installing..."
+		cp bin/libbass_fx.so /usr/lib/
+	fi
 }
 
+function OtherDeps
+{
+	apt install libglfw3-dev libgles2-mesa-dev -y
+}
+
+Dotnet
+BassDeps
+OtherDeps
