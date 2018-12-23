48a824a81cdc0b1a04292da947b2aae553006541
Alexmitter <compaq@live.de>
2018-12-23 21:03:38 +0100

    Add a patch mechanism to build on dotnet2.1

--- scripts/build_osulazer.sh
+++ scripts/build_osulazer.sh
@@ -1,5 +1,12 @@
 #/bin/bash
 
+patch()
+{
+	#Will change the netcoreapp varibale in all .csproj 
+	find . -name '*.csproj' -print0 | xargs -0 sed -i 's/netcoreapp2.2/netcoreapp2.1/g'
+	
+}
+
 echo osu!lazer installer for linux
 
 echo This script will clone osu!lazer and build it.
@@ -9,6 +16,8 @@ git clone --recurse-submodules https://github.com/ppy/osu.git
 cd osu/
 #git submodule update --init
 
+patch
+#find . -name '*.csproj' -print0 | xargs -0 sed -i 's/netcoreapp2.2/netcoreapp2.1/g' #To Compile since a change on osu's side need dotnet 2.1
 
 dotnet restore
 dotnet build -c Release
