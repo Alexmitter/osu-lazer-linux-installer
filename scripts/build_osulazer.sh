745ce20a00a59a80e8a1115fd138dc0e5389a821
Alexmitter <compaq@live.de>
2018-12-30 14:05:52 +0100

    Fixed wrong detection of dotnet version

--- scripts/build_osulazer.sh
+++ scripts/build_osulazer.sh
@@ -5,7 +5,7 @@ patch()
 	string=$(dotnet --version)
 	
 	case "$string" in 
-	*2.1*)
+	*2.1.*)
 		echo "INFO: Dotnet 2.1 detected, will patch csproj files"
 		#Will change the netcoreapp varibale in all .csproj 
 		find . -name '*.csproj' -print0 | xargs -0 sed -i 's/netcoreapp2.2/netcoreapp2.1/g'
