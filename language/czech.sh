28bec250f9f0b05fb2aa5a418d48adc1cd9ecf32
CornyjK <cleverline1mc@gmail.com>
2020-05-07 10:52:49 +0200

    Add Czech language

---
+++ language/czech.sh
@@ -0,0 +1,35 @@
+#/bin/sh
+
+#language pack for the dialoge menu
+
+#Generic
+NOTIMPLEMENTED="Dosud není implementováno"
+INSTALLPACKAGES="Instalování "
+
+#start.sh
+TITLE="Skript pro sestavení a běh osu!lazer"
+WINDOW="Menu"
+RUN="Spustit osu!lazer"
+COMPILE="Naklonovat a zkompilovat osu!lazer"
+PKG="Udělat spustitelný soubor Linuxu (není třeba)"
+DEPENDS="Nainstalovat závislosti"
+CHOOSE="Vyberte jednu z následujících možností:"
+QUICKFIXES="Záplaty"
+QUICKFIXES2="Záplaty pro časté problémy"
+NOOSUFOUND="osulazer nebyl nalezen, nejdříve proveďte sestavení"
+OLDOSUFOUND="starý osulazer nalezen"
+BUILDNEWASK="Chcete odstranit starý osulazer pro sestavení nového"
+SURETODELETE="Jste si jisti, že chcete odstranit vaše staré sestavení osu (potřebuje root) "
+OSUNOTREMOVED="osu neodstraněn"
+ESCPRESSED="[ESC] klávesa stisknuta"
+
+#dependences.sh
+DOTNETDL="Dotnet 2.1 (Placeholder)"
+DEBDEP="Nutnost společných závislostí Debian/Ubuntu"
+INSTALLBASSLIB="Instalování Bass knihoven z vašeho sestaveného osulazer (Placeholder)"
+MULSCREEN="Balíček pro opravu problémů s více monitory"
+
+#Quickfixes.sh
+NOTHINGHERE="Prozatím nic"
+DELFRAMEWORKINI="Odstranit framework.ini, opravuje nespouštění"
+DELFRAMEWORKINI2="Odstraňování framework.ini z ~/.local/share/osu"
