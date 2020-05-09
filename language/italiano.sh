70fb55241ed6a93bb106e68febbe79bf00c0cb6f
Alexmitter <Alexmitter@users.noreply.github.com>
2020-05-09 15:40:38 +0200

    Add appimage text...
    ...and remove dependency install script tastes

--- language/italiano.sh
+++ language/italiano.sh
@@ -24,13 +24,17 @@ SURETODELETE="Sei sicuro di voler cancellare la tua vecchia cartella e build di 
 OSUNOTREMOVED="osu invariato"
 ESCPRESSED="[ESC] Schiacciato"
 
-#dependences.sh
-DOTNETDL="Dotnet 2.1(Placeholder)"
-DEBDEP="Installazione dei pachetti comuni di Debian/Ubuntu richiesti per l'avvio di osu"
-INSTALLBASSLIB="Installazione delle librerie Bass dalla cartella di compilazione di osulazer(Placeholder)"
-MULSCREEN="Pachetto che corregge alcuni errori con più display"
-
 #Quickfixes.sh
 NOTHINGHERE="Per adesso, vuoto"
 DELFRAMEWORKINI="Rimuovi framework.ini, ripara il mancato avvio"
 DELFRAMEWORKINI2="Rimuovendo framework.ini da ~/.local/share/osu"
+
+APPIMG_DOWNLOAD_DIALOG="Download osu!lazer AppImage"
+APPIMG_NEWEST_ALREADY_DOWNLOADED="La versione più recente è già stata scaricata"
+APPIMG_GITHUB_API_LIMIT="Il limite di richieste alle API di GitHub è stato raggiunto, riprova tra qualche minuto"
+APPIMG_ALREADY_DOWNLOADED="Questa versione è già stata scaricata"
+APPIMG_UPDATE_AVAILABLE="Nuova AppImage disponibile, vai su AppImage/Download per aggiornare"
+APPIMG_CHROOT_1="La AppImage di osu!lazer deve essere impostata come eseguibile"
+APPIMG_CHROOT_2="Eseguiremo sudo chmod +x $path2, se non ti fidi di questo script puoi farlo tu stesso"
+APPIMG_CHROOT_3="Premi invio quando sei pronto"
+APPIMG_CHROOT_ERROR="Errore: AppImage non è un eseguibile"
